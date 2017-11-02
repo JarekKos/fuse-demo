24d13bcda4dabd3e6da7b9b18ad18808593e1823
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-11-02 19:30:41 +0100

    Add new service and module to the App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { DataLoaderService } from './data-loader.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DataLoaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

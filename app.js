ae6e22cd294b86c0757675a6c3d2ad60b33347a9
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-11-02 22:46:35 +0100

    Minor update to server app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
 
 app.listen(port);
 

dba8513634d5eb36f1e8daf89ed1caddc23903b3
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-11-03 13:51:00 +0100

    Add file with questions

---
+++ src/app/questions.ts
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    text: 'Is it true that Pepsi produced a can that omits “Under God” from the Pledge of Allegiance?',
+  },
+  {
+    text: 'Is Pepsi ONE still available for purchase?',
+  },
+  {
+    text: 'Is Pepsi Throwback still available?',
+  },
+  {
+    text: 'Where can I purchase a particular product?',
+  },
+  {
+    text: 'Students looking for Pepsi information?',
+  },
+  {
+    text: 'How do I serve Pepsi?',
+  },
+  {
+    text: 'How do I apply for a job at Pepsi?',
+  },
+  {
+    text: 'What is PepsiCo doing to promote recycling?',
+  },
+  {
+    text: 'What is Pepsi doing in order to better the environment?',
+  },
+  {
+    text: 'What do the dates on the packaging mean?',
+  },
+];
+
+export default questions;

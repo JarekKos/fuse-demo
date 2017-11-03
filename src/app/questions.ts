d5eeb1277906f1d30af69eb440e2c2bb7e05afd3
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-11-03 15:49:28 +0100

    Update to file with questions

--- src/app/questions.ts
+++ src/app/questions.ts
@@ -1,33 +1,43 @@
 const questions = [
   {
     text: 'Is it true that Pepsi produced a can that omits “Under God” from the Pledge of Allegiance?',
+    tags: ['firstAnswer']
   },
   {
     text: 'Is Pepsi ONE still available for purchase?',
+    tags: ['secondAnswer']
   },
   {
     text: 'Is Pepsi Throwback still available?',
+    tags: ['thirdAnswer']
   },
   {
     text: 'Where can I purchase a particular product?',
+    tags: ['fourthAnswer']
   },
   {
     text: 'Students looking for Pepsi information?',
+    tags: ['fifthAnswer']
   },
   {
     text: 'How do I serve Pepsi?',
+    tags: ['sixthAnswer']
   },
   {
     text: 'How do I apply for a job at Pepsi?',
+    tags: ['seventhAnswer']
   },
   {
     text: 'What is PepsiCo doing to promote recycling?',
+    tags: ['eighthAnswer']
   },
   {
     text: 'What is Pepsi doing in order to better the environment?',
+    tags: ['ninthAnswer']
   },
   {
     text: 'What do the dates on the packaging mean?',
+    tags: ['tenthAnswer']
   },
 ];
 

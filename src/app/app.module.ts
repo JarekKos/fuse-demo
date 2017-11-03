6fa5b3ace4eb2f8f4922fd9303bd48f4f110f742
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-11-03 11:34:51 +0100

    Removing Forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
@@ -13,7 +12,6 @@ import { DataLoaderService } from './data-loader.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
   ],
   providers: [DataLoaderService],
   bootstrap: [AppComponent]

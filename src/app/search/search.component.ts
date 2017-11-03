6ddf1ec7acdf1e67ae42bb97840bad0ed8dc93c3
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-11-03 15:53:11 +0100

    Update to search Component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,16 +9,28 @@ import 'rxjs/add/operator/debounceTime';
 })
 export class SearchComponent {
 
-  list = [];
+  questions = [];
+
+  question = null;
+  answer = null;
 
   constructor(private dataLoader: DataLoaderService) { }
 
-  onKeyPress(value) {
-    this.dataLoader.getData(value).subscribe(
+  onKeyUp() {
+    this.dataLoader.getQuestions(this.question).subscribe(
       data => {
-        this.list = data;
+        this.questions = data;
       }
     );
+  }
+
+  onSubmit(fm) {
+    console.log(fm);
+  }
+
+  optionSelected(ev) {
+    this.question = ev.option.viewValue;
+    this.answer = this.dataLoader.getAnswers(ev.option.value[0]);
   }
 
 }

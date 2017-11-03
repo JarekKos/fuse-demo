ca220ced2a2ca9654b51d6e682f56dd48c5481df
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-11-03 15:52:49 +0100

    Adding FormModule and MaterialAutoComplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { DataLoaderService } from './data-loader.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { DataLoaderService } from './data-loader.service';
   ],
   imports: [
     BrowserModule,
+    MatAutocompleteModule,
+    FormsModule,
   ],
   providers: [DataLoaderService],
   bootstrap: [AppComponent]

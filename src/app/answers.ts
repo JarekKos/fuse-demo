615f798761235ffac201f8f0ee00f43accf0d1c6
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-11-03 15:49:07 +0100

    Create file with answers

---
+++ src/app/answers.ts
@@ -0,0 +1,69 @@
+const answers = [
+  {
+    text: 'No. You may have received an erroneous message about a "patriotic can" that Pepsi allegedly ' +
+    'produced with an edited version of America’s Pledge of Allegiance. The truth is, ' +
+    'Pepsi never produced such a can. In fact, this is a hoax that has been circulating on the Internet ' +
+    'for more than ten years. A patriotic package used in 2001 by Dr. Pepper (which is not a part of PepsiCo) ' +
+    'was inappropriately linked to Pepsi. Thanks for giving us the chance to clarify the situation and please ' +
+    'feel free to share this message with anyone else who may have received the erroneous email. ' +
+    'Details of the hoax can be found at http://www.snopes.com/politics/business/undergod.asp.',
+    tags: ['firstAnswer']
+  },
+  {
+    text: 'While Pepsi ONE has been discontinued, we do want to share that Diet Pepsi has been reformulated ' +
+    'and aspartame has been removed. With its light, crisp taste, Diet Pepsi gives you all the refreshment ' +
+    'you need - with zero sugar, zero calories and zero carbs.',
+    tags: ['secondAnswer']
+  },
+  {
+    text: 'Pepsi Throwback is now Pepsi Cola Made With Real Sugar. It’s the same great formula, ' +
+    'just updated name and graphics! To see where you can purchase Pepsi Cola Made With Real Sugar, ' +
+    'click on the Where to Buy tab, or visit http://itemlocator.pepsico.com/pepsi',
+    tags: ['thirdAnswer']
+  },
+  {
+    text: 'To see a list of stores that have sold your favorite products in the last 30 days, ' +
+    'go to http://www.pepsicobeveragefacts.com/home/wheretobuy.',
+    tags: ['fourthAnswer']
+  },
+  {
+    text: 'Thanks for your interest in Pepsi! If you’re a student looking for information on one of our ' +
+    'brands or products, you should check out our Corporate website. Financial information such as our annual ' +
+    'reports can be obtained by clicking on the “Investors” tab. Just please know that some information you ' +
+    'may be looking for will be unavailable due to its proprietary nature (marketing plans, ' +
+    'advertising strategies, exact product formulations, processing technology, etc.).<br/>' +
+    'If you’re looking for information on the history and story of Pepsi-Cola, click here.<br/>' +
+    'Unfortunately, we’re unable to research and prepare special answers for the numerous requests we ' +
+    'receive each day. However, your public library or the internet may be excellent research sources on ' +
+    'this subject.',
+    tags: ['fifthAnswer']
+  },
+  {
+    text: 'https://www.iwanttoservepepsi.com/aaws/',
+    tags: ['sixthAnswer']
+  },
+  {
+    text: 'In order to apply for a job at PepsiCo, head to http://www.pepsico.com/Careers.html, ' +
+    'our all-inclusive stop for job searching, résumé submissions and more.',
+    tags: ['seventhAnswer']
+  },
+  {
+    text: 'To learn about PepsiCo recycling initiatives and Recycle Rally, click here.?',
+    tags: ['eighthAnswer']
+  },
+  {
+    text: 'Read detailed accounts of PepsiCo’s company-wide initiative to protect the earth’s natural ' +
+    'resources through innovation and efficiency at http://www.pepsico.com/Purpose/Environmental-Sustainability.html.',
+    tags: ['ninthAnswer']
+  },
+  {
+    text: 'All of our products have a "Best taste date," which is very different from an expiration date ' +
+    'often found on perishable items such as dairy and meat products.  Our "Best taste date" is a self-imposed ' +
+    'indicator that has been listed on the underside of our cans, shoulders of bottles and sides of paperboard ' +
+    'wraps since 1994.  We provide this information simply to help ensure that our products are consumed at ' +
+    'their absolute best flavor.',
+    tags: ['tenthAnswer']
+  },
+];
+
+export default answers;

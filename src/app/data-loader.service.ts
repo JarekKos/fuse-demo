4c3336cc83070656229f42fc3a154219368ae115
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-11-06 10:47:38 +0100

    Updates to DataLoader service

--- src/app/data-loader.service.ts
+++ src/app/data-loader.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import * as Fuse from 'fuse.js';
 import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/map';
+import * as Fuse from 'fuse.js';
 
 import questions from './questions';
 import answers from './answers';
@@ -14,19 +13,26 @@ export class DataLoaderService {
   private answers: Fuse = null;
 
   constructor() {
-    this.questions = new Fuse(questions, {keys: ['text'],
-      threshold: 0.3, tokenize: true,
-      shouldSort: true, matchAllTokens: true});
-    this.answers = new Fuse(answers, {keys: ['tags'], threshold: 0.1});
+    this.questions = new Fuse(questions, {
+        keys: ['text'],
+        threshold: 0.3,
+        tokenize: true,
+        shouldSort: true,
+        matchAllTokens: true
+    });
+    this.answers = new Fuse(answers, {
+      keys: ['tags'],
+      threshold: 0.1
+    });
   }
 
-  getQuestions(text) {
+  getQuestions(text: string): Observable<Array<{text: string, tags: Array<string>}>> {
     return Observable.of(this.questions.search(text));
   }
 
-  getAnswers(tag) {
+  getAnswers(tag: string): Observable<string> | Observable<null> {
     return Observable.of(this.answers.search(tag)).map(answerArr => {
-      return Array.isArray(answerArr) && answerArr[0] ? answerArr[0]['text'] : [];
+      return Array.isArray(answerArr) && answerArr[0] ? answerArr[0]['text'] : null;
     });
   }
 
